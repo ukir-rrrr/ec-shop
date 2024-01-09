2f01a3cfceeef4da09430b8a4f95a967e9d2b86b
ukir-rrrr <aorikuaoriku@icloud.com>
2024-01-09 20:50:55 +0900

    Add new feature

--- ecshop_db.sql
+++ ecshop_db.sql
@@ -22,54 +22,43 @@ CREATE TABLE Users (
 CREATE TABLE staff (
     code INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
-    Password VARCHAR(30) NOT NULL
+    Password VARCHAR(255) NOT NULL
 );
 
--- categoriesテーブル
-CREATE TABLE categories (
-    id INT PRIMARY KEY,
-    category_id INT UNIQUE NOT NULL,
-    category_name VARCHAR(255) NOT NULL
-);
-
--- productsテーブル
 CREATE TABLE products (
-    id INT PRIMARY KEY,
-    category_id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     stock INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    image_path VARCHAR(255)
 );
 
-
-CREATE TABLE categories (
-    id INT PRIMARY KEY,
-    category_id INT(11) NOT NULL,
-    category_name VARCHAR(255) NOT NULL
-);
-
--- productsテーブル
-CREATE TABLE products (
-    id INT PRIMARY KEY,
-    category_id VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    image_path VARCHAR(255),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
-);
-
+-- 問い合わせ
 CREATE TABLE inquiries (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 売上
+CREATE TABLE Sales (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT,
+    OrderDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    TotalAmount INT,
+    Last_name VARCHAR(50),
+    First_name VARCHAR(50),
+    Zipcode VARCHAR(7),
+    Address TEXT,
+    PaymentMethod VARCHAR(20),
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ProductID) REFERENCES products(id)
 );
